cb8f2d4c365f881b964bb24fbe5cfc8d788f7364
Ansh11023 <ansh91005@gmail.com>
2024-10-19 09:59:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let display = document.getElementById('display');
+let currentInput = '';
+let operator = '';
+let previousInput = '';
+
+function appendNumber(number) {
+    currentInput += number;
+    display.innerText = currentInput;
+}
+
+function appendOperator(op) {
+    if (currentInput === '') return;
+    if (previousInput !== '') {
+        calculate();
+    }
+    operator = op;
+    previousInput = currentInput;
+    currentInput = '';
+}
+
+function clearDisplay() {
+    currentInput = '';
+    previousInput = '';
+    operator = '';
+    display.innerText = '0';
+}
+
+function deleteLast() {
+    currentInput = currentInput.slice(0, -1);
+    display.innerText = currentInput || '0';
+}
+
+function calculate() {
+    let result;
+    const prev = parseFloat(previousInput);
+    const current = parseFloat(currentInput);
+
+    if (isNaN(prev) || isNaN(current)) return;
+
+    switch (operator) {
+        case '+':
+            result = prev + current;
+            break;
+        case '-':
+            result = prev - current;
+            break;
+        case '*':
+            result = prev * current;
+            break;
+        case '/':
+            result = prev / current;
+            break;
+        default:
+            return;
+    }
+
+    currentInput = result;
+    operator = '';
+    previousInput = '';
+    display.innerText = result;
+}
